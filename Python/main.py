8d0d3c6d5a3bf0da805dc866e939d378e9d85dea
ndr0n <esquilofreniko@gmail.com>
2022-09-16 00:11:47 +0100

    First Commit

---
+++ Python/main.py
@@ -0,0 +1,102 @@
+import numpy as np
+import time
+from osc import OscClient
+from osc import OscServer
+from keras1 import NeuralNetRegression
+
+#parameters
+inputSize = 1200
+outputSize = 1200
+nHidden = 3
+nNodes = 10
+epochs = 1000
+
+trained = 0
+nExamples = 0
+x = np.array([0])
+y = np.array([0])
+nn = NeuralNetRegression(np.zeros((1,inputSize)), np.zeros((1,outputSize)), nHidden, nNodes)
+oscclient = OscClient("127.0.0.1", 12000, inputSize, outputSize)
+oscclient.sendMsg(np.array([[0],[0]]).tolist(), '/keras/training')
+oscserver = OscServer("127.0.0.1", 6448, inputSize, outputSize)
+print("press ctrl+c: quit")
+
+while True:
+    time.sleep(0.1)
+    if trained == 1:
+        pred = np.array([oscserver.pred])
+        if(pred.shape[1] == outputSize):
+            yout = nn.predict(pred)
+            yout = yout.tolist()
+            oscclient.sendMsg(yout, '/keras/yout')
+            oscserverxin = oscserver.xin
+    if oscserver.addexample == 1:
+        if(oscserver.xin.size == inputSize & oscserver.yin.size == outputSize):
+            if(nExamples==0):
+                x = oscserver.xin
+                y = oscserver.yin
+            else:
+                x = np.vstack((x,oscserver.xin))
+                y = np.vstack((y,oscserver.yin))
+            nExamples += 1
+            print("nExamples:", nExamples)
+        else:
+            print("Error Adding Example - Wrong Size! xSize: " + str(oscserver.xin.size) + " | ySize: " + str(oscserver.yin.size))
+        oscserver.addexample = 0
+        pass
+    if oscserver.delexample == 1:
+        nExamples -= 1
+        x = x[:-1]
+        y = y[:-1]
+        print(x)
+        print(y)
+        if(nExamples<0):nExamples=0
+        print("Removed Example")
+        print("nExamples:", nExamples)
+        oscserver.delexample = 0
+        pass
+    if oscserver.delall == 1:
+        nExamples = 0
+        x = np.array([0])
+        y = np.array([0])
+        print("Cleared Examples")
+        print("nExamples:", nExamples)
+        oscserver.delall = 0
+    if oscserver.train == 1:
+        # train
+        if(nExamples > 1):
+            oscclient.sendMsg(np.array([[1],[1]]).tolist(),'/keras/training')
+            print("Training Neural Network...")
+            print("nExamples:",nExamples,"Epochs:",oscserver.epochs)
+            nn.train(x,y,nExamples,oscserver.epochs)
+            print("Finished Training Neural Network")
+            oscclient.sendMsg(np.array([[0],[0]]).tolist(),'/keras/training')
+            trained = 1
+            oscserver.delall = 1
+            oscserver.train = 0
+        else:
+            print("Error Training - Not Enough Examples.")
+            oscserver.train = 0
+        pass
+    if oscserver.trainNew == 1:
+        # train New
+        if(nExamples > 1):
+            nn = NeuralNetRegression(x,y,nHidden,nNodes)
+            print("Training New Neural Network...")
+            print("nExamples:",nExamples,"Epochs:",oscserver.epochs)
+            oscclient.sendMsg(np.array([[1],[1]]).tolist(),'/keras/training')
+            nn.train(x,y,nExamples,oscserver.epochs)
+            print("Finished Training New Neural Network")
+            oscclient.sendMsg(np.array([[0],[0]]).tolist(),'/keras/training')
+            trained = 1
+            oscserver.trainNew = 0
+        else:
+            print("no Examples found. Need atleast 2 Examples to Train")
+            oscserver.trainNew = 0
+        pass
+    if oscserver.quit == 1:
+        oscserver.server.shutdown()
+        quit()
+        break
+    else:
+        pass
---
+++ Python/osc.py
@@ -0,0 +1,130 @@
+import argparse
+import threading
+import numpy as np
+from pythonosc import dispatcher
+from pythonosc import udp_client
+from pythonosc import osc_server
+from pythonosc.osc_message_builder import OscMessageBuilder
+
+class OscClient:
+    inputSize = 1200
+    outputSize = 1200
+
+    def __init__(self,ip,port, input, output):
+        self.ip = ip
+        self.port = port
+        self.inputSize = input
+        self.outputSize = output
+        # self.address = address
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--ip", default=ip)
+        parser.add_argument("--port", type=int, default=port)
+        args = parser.parse_args()
+        self.client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+    def sendMsg(self,msg,msgAdress):
+        builder = OscMessageBuilder(address=msgAdress)
+        for v in msg[0]: 
+            builder.add_arg(v)
+        out = builder.build()
+        # print("sent osc message to",self.ip,"on port",self.port,"with address",self.address)
+        self.client.send(out)
+
+class OscServer:
+    inputSize = 2400
+    outputSize = 2400
+
+    def getPred(self,unused_addr,*args):
+        self.pred = np.array(args)
+        # if arraynum == 0:
+        #     self.prePred = args
+        # if arraynum >= 1:
+        #     self.prePred = np.vstack((self.prePred,args))
+        # if arraynum == 14:
+        #     test = self.prePred.ravel()
+        #     if(test.size == self.outputSize):
+        #         self.pred = test
+        #         print("P:",self.pred.size)
+        #     else:
+        #         print("Error Receiving P - Wrong Size")
+
+    def getX(self,unused_addr, *args):
+        self.xin = np.array(args)
+        # if arraynum == 0:
+        #     self.prex = args
+        # if arraynum >= 1:
+        #     self.prex = np.vstack((self.prex,args))
+        # if arraynum == 14:
+        #     test = self.prex.ravel()
+        #     if(test.size == self.inputSize):
+        #         self.xin = test
+        #         print("X:", self.xin.size)
+        #     else:
+        #         print("Error Receiving X - Wrong Size")
+
+    def getY(self,unused_addr, *args):
+        self.yin = np.array(args)
+        # if arraynum == 0:
+        #     self.prey = args
+        # if arraynum >= 1:
+        #     self.prey = np.vstack((self.prey,args))
+        # if arraynum == 14:
+        #     test = self.prey.ravel()
+        #     if(test.size == self.outputSize):
+        #         self.yin = test
+        #         print("Y:", self.yin.size)
+        #     else:
+        #         print("Error Receiving Y - Wrong Size")
+    
+    def addExample(self,unused_addr,*args):
+        self.addexample = 1
+    
+    def delExample(self,unused_addr,*args):
+        self.delexample = 1
+    
+    def delAll(self,unused_addr,*args):
+        self.delall = 1
+    
+    def trainModel(self,unused_addr,*args):
+        self.train = 1
+
+    def trainNewModel(self,unused_addr,*args):
+        self.trainNew = 1
+
+    def close(self,unused_addr,*args):
+        self.quit = 1
+
+    def getEpochs(self,*args):
+        self.epochs = args[1]
+        print("Epochs:",self.epochs)
+        
+    def __init__(self, ip, port, input, output):
+        self.inputSize = input
+        self.outputSize = output
+        self.epochs = 1000
+        self.addexample = 0
+        self.delexample = 0
+        self.delall = 0
+        self.train = 0
+        self.trainNew = 0
+        self.quit = 0
+        self.pred = np.array([0])
+        self.xin = np.array([0])
+        self.yin = np.array([0])
+        self.prePred = np.array([0])
+        self.prex = np.array([0])
+        self.prey = np.array([0])
+        self.dispatcher = dispatcher.Dispatcher()
+        self.dispatcher.map('/keras/xin', self.getX)
+        self.dispatcher.map('/keras/yin',self.getY)
+        self.dispatcher.map('/keras/predin',self.getPred)
+        self.dispatcher.map('/keras/epochs',self.getEpochs)
+        self.dispatcher.map('/keras/addexample',self.addExample)
+        self.dispatcher.map('/keras/delexample',self.delExample)
+        self.dispatcher.map('/keras/delall',self.delAll)
+        self.dispatcher.map('/keras/train',self.trainModel)
+        self.dispatcher.map('/keras/trainnew',self.trainNewModel)
+        self.dispatcher.map('/keras/quit',self.close)
+        self.server = osc_server.ThreadingOSCUDPServer((ip, port), self.dispatcher)
+        server_thread = threading.Thread(target=self.server.serve_forever)
+        server_thread.start()
